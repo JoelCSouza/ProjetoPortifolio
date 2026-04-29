15c98f1e3682db52b816caa3ae507c503bf21b84
JoelCSouza <joel1719souza@gmail.com>
2026-04-28 22:21:03 -0300

    atualizei o botao tema, confirmação do formulario

--- script.js
+++ script.js
@@ -11,3 +11,52 @@ function hideMenuResponsivo(){
             menuResponsivo.style.display = "none";
             menuDesktop.style.display = "flex";
 }
+
+/*botao tema*/
+const tema = document.getElementById("theme-toggle");
+
+tema.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+
+    const temaAtual = document.body.classList.contains("light") ? "light" : "dark";
+    localStorage.setItem("tema", temaAtual);
+});
+
+window.addEventListener("load", () => {
+  const temaSalvo = localStorage.getItem("tema");
+   if (temaSalvo === "light") {
+    document.body.classList.add("light");
+  }
+});
+
+/*Validação usuário */
+const form = document.getElementById("formularioContato");
+
+function enviarFormulario(){
+    alert("Mensagem enviada com sucesso!");
+    form.reset()
+}
+
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nome = document.getElementById("nome-usuario").value.trim();
+    const email = document.getElementById("emailUsuario").value.trim();
+    const mensagem = document.getElementById("mensagem").value.trim();
+    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!nome || !email || !mensagem) {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    if (!emailValido.test(email)) {
+        alert("Digite um email válido!!");
+        return;
+    }
+
+    enviarFormulario();
+});
+
+
